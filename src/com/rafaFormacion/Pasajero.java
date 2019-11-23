d327815688ec80804906664766f020a5a9328787
Rafiki81 <rperezbeato@gmail.com>
2019-11-24 00:09:59 +0100

    Commit Inicial:
    Creado el esqueleto del proyecto.
    Creada la Clase Pasajero con sus getter y setter (Ejercicio 1 de la PAC)

---
+++ src/com/rafaFormacion/Pasajero.java
@@ -0,0 +1,45 @@
+package com.rafaFormacion;
+
+public class Pasajero {
+    private String nombre;
+    private int asientoAsignado;
+    private boolean tieneTarjeta;
+
+    public Pasajero(String nombre, int asientoAsignado) {
+        this.nombre = nombre;
+        this.asientoAsignado = asientoAsignado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAsientoAsignado() {
+        return asientoAsignado;
+    }
+
+    public void setAsientoAsignado(int asientoAsignado) {
+        this.asientoAsignado = asientoAsignado;
+    }
+
+    public boolean isTieneTarjeta() {
+        return tieneTarjeta;
+    }
+
+    public void setTieneTarjeta(boolean tieneTarjeta) {
+        this.tieneTarjeta = tieneTarjeta;
+    }
+
+    @Override
+    public String toString() {
+        return "Pasajero{" +
+                "nombre='" + nombre + '\'' +
+                ", asientoAsignado=" + asientoAsignado +
+                ", tieneTarjeta=" + tieneTarjeta +
+                '}';
+    }
+}
---
+++ src/com/rafaFormacion/Pasajeros.java
@@ -0,0 +1,4 @@
+package com.rafaFormacion;
+
+public class Pasajeros {
+}
