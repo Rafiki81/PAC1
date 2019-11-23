f6edf8bfbbb44d46e29eacf591111e9095dd278e
Rafiki81 <rperezbeato@gmail.com>
2019-11-24 00:15:38 +0100

    Creada la clase Pasajeros Ejercicio 2 de la PAC.

--- src/com/rafaFormacion/Pasajeros.java
+++ src/com/rafaFormacion/Pasajeros.java
@@ -1,4 +1,41 @@
 package com.rafaFormacion;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Pasajeros {
+
+    private List<Pasajero> pasajeros;
+    private int numeroPasajeros;
+
+    public Pasajeros(int numeroPasajeros) {
+        this.numeroPasajeros = numeroPasajeros;
+    }
+
+    public boolean tieneMasPasajeros(){
+
+        if (pasajeros.size()>0){
+            return true;
+        }else  {
+            return false;
+        }
+    }
+
+    public Pasajero siguientePasajero(){
+
+       return pasajeros.get(0);
+
+    }
+
+    public void addPasajero(Pasajero pasajero){
+
+        pasajeros.add(pasajero);
+
+    }
+
+    public void desordenar(){
+
+        Collections.shuffle(pasajeros);
+
+    }
 }
